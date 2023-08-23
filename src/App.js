7ceda5317da329ea3713a06d85320800d93348a0
Sarah Gilbreath <thenoiseinspace@gmail.com>
2023-08-22 21:15:57 -0400

    day two

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          Generalized Observation and Reflection Platform (GORP) for Auburn University
         </p>
         <a
           className="App-link"
